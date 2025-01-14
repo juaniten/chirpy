61289d681055b317306b97cd7b50a83b20f42116
juaniten <juanguijarroferreiro@gmail.com>
2025-01-14 16:58:09 +0100

    added health handler

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	const port = "8080"
+	const rootPath = "."
+
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(rootPath))))
+	serveMux.HandleFunc("/healthz", healthHandler)
+
+	log.Printf("Listing on port %s: serving files from `%s`.\n", port, rootPath)
 	server := http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 	server.ListenAndServe()
+}
+
+func healthHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	bodyContents := "OK"
+	w.Write([]byte(bodyContents))
 }
